12007925795082b22af50b22bc34efe8f1405dda
Nathan <nathan.ly.tang@gmail.com>
2021-03-22 22:49:22 -0600

    Feat: base URL and sidebar

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,41 +1,25 @@
 const { description } = require('../../package')
 
 module.exports = {
-    /**
-     * Ref：https://v1.vuepress.vuejs.org/config/#title
-     */
-    title: 'Nathan\'s Documentation',
-    /**
-     * Ref：https://v1.vuepress.vuejs.org/config/#description
-     */
+    base: '/documentation/',
+    title: 'Nathan\'s Docs',
     description: 'Documentation for all of Nathan\'s public repositories',
-
-    /**
-     * Extra tags to be injected to the page HTML `<head>`
-     *
-     * ref：https://v1.vuepress.vuejs.org/config/#head
-     */
     head: [
         ['meta', { name: 'theme-color', content: '#00eba4' }],
         ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
         ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
     ],
 
-    /**
-     * Theme configuration, here is the default theme configuration for VuePress.
-     *
-     * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-     */
     themeConfig: {
         repo: '',
         editLinks: false,
         docsDir: '',
         editLinkText: '',
-        lastUpdated: false,
+        lastUpdated: true,
         nav: [
             {
                 text: 'Projects',
-                link: '/projects/introduction.md',
+                link: '/projects/',
             },
             {
                 text: 'Portfolio',
@@ -47,34 +31,31 @@ module.exports = {
             }
         ],
         sidebar: {
-            '/projects/': [
-                {
-                    title: 'Projects',
-                    collapsable: false,
-                    children: [
-                        '/projects/introduction.md'
-                    ]
-                },
+            '/projects/cloudflare-ddns': [
                 {
                     title: 'Cloudflare-DDNS',
-                    collapsable: true,
+                    collapsable: false,
                     children: [
                         '/projects/cloudflare-ddns/introduction.md',
                         '/projects/cloudflare-ddns/setup.md',
                         '/projects/cloudflare-ddns/multiple_domain.md'
                     ]
                 },
+            ],
+            '/projects/inhabit': [
                 {
                     title: 'Inhabit',
-                    collapsable: true,
+                    collapsable: false,
                     children: [
                         '/projects/inhabit/introduction.md',
                         '/projects/inhabit/setup.md'
                     ]
                 },
+            ],
+            '/projects/steve/': [
                 {
                     title: 'Steve',
-                    collapsable: true,
+                    collapsable: false,
                     children: [
                         '/projects/steve/introduction.md',
                         '/projects/steve/getting_started.md',
@@ -82,9 +63,11 @@ module.exports = {
                         '/projects/steve/additional_configuration.md'
                     ]
                 },
+            ],
+            '/projects/wg-cli': [
                 {
                     title: 'wg-cli',
-                    collapsable: true,
+                    collapsable: false,
                     children: [
                         '/projects/wg-cli/introduction.md',
                         '/projects/wg-cli/install.md',
@@ -95,11 +78,4 @@ module.exports = {
         }
     },
 
-    /**
-     * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-     */
-    plugins: [
-        '@vuepress/plugin-back-to-top',
-        '@vuepress/plugin-medium-zoom',
-    ]
 }
